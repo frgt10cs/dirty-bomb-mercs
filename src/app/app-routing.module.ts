0accbd7e7f8612b3441d695933ef94b1876c6962
frgt10cs <ivan_gavrilov_2016@bk.ru>
2020-08-03 20:47:10 +0300

    build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,10 @@ import {AbilitiesComponent} from './core/abilities/abilities.component';
 const appName = 'dirty-bomb-mercs';
 
 const routes: Routes = [
-  {path: `${appName}/`, redirectTo: '/mercs', pathMatch: 'full'},
-  {path: `${appName}/mercs`, component: MercsComponent},
-  {path: `${appName}/weapons/:mercId`, component: WeaponsComponent},
-  {path: `${appName}/abilities/:mercId`, component: AbilitiesComponent}
+  {path: ``, redirectTo: `/mercs`, pathMatch: 'full'},
+  {path: `mercs`, component: MercsComponent},
+  {path: `weapons/:mercId`, component: WeaponsComponent},
+  {path: `abilities/:mercId`, component: AbilitiesComponent}
 ];
 
 @NgModule({
